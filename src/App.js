426ed9458e4a43613330e600e37aae6f20b330e9
Kevin Huang <yellow.khaos@gmail.com>
2019-02-09 16:50:01 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    this.props.dispatch(addItem());
   }
 
   render() {
